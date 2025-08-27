7f0d84ff18d6eb569e0fb52b2aea0cbe04de1f19
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-08-27 22:10:59 +0500

    Make linear search generic for multiple data types

--- Array/Searching/1 linear search.cpp
+++ Array/Searching/1 linear search.cpp
@@ -1,35 +1,86 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int linear_search(int arr[], int size, int target){
-	
-	for (int i = 0; i < size; i++){
-		if (arr[i] == target){
+template <typename T>
+int linear_search(T arr[], int size, T target)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] == target)
+		{
 			return i;
 		}
 	}
 	return -1;
 }
 
-int main(){
-	
-	int size;
-	
-	cout << "Enter the size of Array : ";
-	cin >> size;
-	
-	int arr[size];
-	
-	cout << "\n---- Enter the elements of Array  ----\n";
-	for(int i = 0 ; i < size ; i++){
-		cout << "\nEnter element " << i+1 << " : ";
+template <typename T>
+void run_search(int size)
+{
+	T arr[size];
+
+	cout << "\n---- Enter the elements of Array ----\n";
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Enter element " << i + 1 << " : ";
 		cin >> arr[i];
 	}
-	
-	int target ;
+
+	T target;
 	cout << "\nEnter the element to search in the Array : ";
 	cin >> target;
-	
-	int index = linear_search(arr,size,target);
-	cout << "\n" << target << " is found at index " << index; 
+
+	int index = linear_search(arr, size, target);
+
+	if (index != -1)
+	{
+		cout << "\n"
+		     << target << " is found at index " << index << endl;
+	}
+	else
+	{
+		cout << "\n"
+		     << target << " is not found in the array." << endl;
+	}
+}
+
+int main()
+{
+	int size;
+	cout << "Enter the size of Array : ";
+	cin >> size;
+
+	cout << "\nChoose data type of array:\n";
+	cout << "1. int\n";
+	cout << "2. float\n";
+	cout << "3. double\n";
+	cout << "4. char\n";
+	cout << "5. string\n";
+
+	int choice;
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		run_search<int>(size);
+		break;
+	case 2:
+		run_search<float>(size);
+		break;
+	case 3:
+		run_search<double>(size);
+		break;
+	case 4:
+		run_search<char>(size);
+		break;
+	case 5:
+		run_search<string>(size);
+		break;
+	default:
+		cout << "Invalid choice!" << endl;
+	}
+
+	return 0;
 }
