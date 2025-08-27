c027529d4fc75015769f8acbf2b0c9bbc1fb5bcd
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-08-27 23:41:09 +0500

    updated binary search using user input

--- Array/Searching/2 binary search.cpp
+++ Array/Searching/2 binary search.cpp
@@ -6,13 +6,13 @@ int binary_search(int arr[], int size, int target) {
 
     while (start <= end) {
         int mid = start + (end - start) / 2; 
-        // Hum (start+end)/2 bhi kar saktay thay lekin agar start or end ki value INT_MAX hui to overflow ho jayega is lie ye formula lagaya hai ye bhi same hi formula hai lekin optimized
+        // Hum mid = (start+end)/2 bhi kar saktay thay lekin agar start or end ki value INT_MAX hui to overflow ho jayega is lie ye formula lagaya hai ye bhi same hi formula hai lekin optimized
 
         if (arr[mid] == target) {
             return mid;
         }
 		
-		if (arr[mid] < target) {
+        if (arr[mid] < target) {
             start = mid + 1;  
         } else {
             end = mid - 1;    
@@ -23,16 +23,34 @@ int binary_search(int arr[], int size, int target) {
 }
 
 int main() {
-    int arr[] = {4, 15, 17, 23, 32, 47, 52, 80};
-    int target = 52;
-    int size = 8;
+    
+    int size;
+    cout << "Enter the size of the Array : ";
+    cin >> size;
+    
+    int arr[size];
+
+    cout << "\n---- Enter the elements of Array (in ascending order)----\n";
+    for (int i = 0; i < size; i++) {
+        cout << "\nEnter element " << i + 1 << " : ";
+        cin >> arr[i];
+
+        while (i > 0 && arr[i-1] > arr[i]) {   
+            cout << "\n" << arr[i-1] << " is greater than " << arr[i] << " please enter any other number greater than " << arr[i - 1] << " : ";
+            cin >> arr[i];
+        }
+    }
+	
+    int target;
+    cout << "\nEnter the targeted element : ";
+    cin >> target;
 
     int idx = binary_search(arr, size, target);
 
     if (idx != -1) {
-        cout << "Element found at index : " << idx;
+        cout << "\nElement found at index : " << idx;
     } else {
-        cout << "Element not found!";
+        cout << "\nElement not found!";
     }
 
     return 0;
