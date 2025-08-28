aa6f3c71e7f4c18ff15d21bf305b3f5b7feee80b
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-08-28 20:18:07 +0500

    Added insertion algorithm for an Array

---
+++ Array/Insertion and Deletion/1 insertion.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void insertFunction(int arr[], int newSize, int element, int index){
+
+	for(int i = newSize;i > index ; i--){
+		arr[i] = arr[i-1];
+	}
+	
+	arr[index] = element;
+
+
+}
+
+void printArray(int arr[], int newSize){
+	for(int i = 0 ; i < newSize ; i++){
+		cout << arr[i];
+	}
+}
+
+int main(){
+
+	cout << "Enter the size of Array : ";
+	int size;
+	cin >> size;
+
+	int newSize = size + 1;
+
+	int arr[newSize];
+
+	cout << "\n---- Enter the elements of Array ----\n";
+	for (int i = 0; i < size; i++) {
+		cout << "\nEnter element " << i + 1 << " : ";
+		cin >> arr[i];
+	}
+
+	int element, index;
+	cout << "\nEnter the element to insert : ";
+	cin >> element;
+	cout << "\nEnter the index at which to insert the element : ";
+	cin >> index;
+
+	insertFunction(arr, newSize, element, index);
+	
+	printArray(arr,newSize);
+	
+	
+
+	return 0;
+}
