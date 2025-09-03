2b9b10f7bb117812c0f191f47987fd5830953265
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-09-04 02:28:18 +0500

    Added push_back function for linked list

---
+++ Linked List/Linked List 2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+	int data;
+	Node* next;
+	
+	Node(int val){
+		data = val;
+		next = nullptr;
+	}
+
+};
+
+class List{
+	Node* head;
+	
+public:
+	List(){
+		head = nullptr;
+	}
+
+	
+	void push_front(int val){
+		Node* newNode = new Node(val);
+		   if(head == nullptr){
+			   head = newNode;
+			   return;
+		   }
+		   newNode->next = head;
+		   head = newNode;
+	}
+	
+	void push_back(int val){
+		Node* newNode = new Node(val);
+		if(head == nullptr){
+			head = newNode;
+		}else{
+			Node* temp = head;
+			
+			while(temp->next != nullptr){
+				temp = temp->next;
+			}
+			
+			temp->next = newNode;
+		}
+	}
+	
+	void print(){
+		Node* temp = head;
+		while(temp != nullptr){
+			cout << temp->data << " -> " ;
+			temp = temp->next;
+		}
+		cout << "nullptr";
+	}
+};
+
+int main(){
+	
+	List ll;
+	ll.push_front(3);
+	ll.push_front(2);
+	ll.push_front(1);
+	ll.push_back(4);
+	ll.push_back(5);
+	ll.print();
+	return 0;
+}
