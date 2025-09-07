fa3bc1935bef80ab21c979a08590e1eb932f2734
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-09-07 17:16:00 +0500

    Added insert() function for linked list

---
+++ Linked List/Linked List 5.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+	int data;
+	Node* next;
+	
+	Node(int val){
+		data = val;
+		next = nullptr;
+	}
+
+};
+
+class List{
+	Node* head;
+	
+public:
+	List(){
+		head = nullptr;
+	}
+
+	
+	void push_front(int val){
+		Node* newNode = new Node(val);
+		   if(head == nullptr){
+			   head = newNode;
+			   return;
+		   }
+		   newNode->next = head;
+		   head = newNode;
+	}
+	
+	void push_back(int val){
+		Node* newNode = new Node(val);
+		if(head == nullptr){
+			head = newNode;
+		}else{
+			Node* temp = head;
+			
+			while(temp->next != nullptr){
+				temp = temp->next;
+			}
+			
+			temp->next = newNode;
+		}
+	}
+	
+	void pop_front(){
+		if(head == nullptr){
+			cout << "List is empty \n";
+			return;
+		}
+		Node* temp = head;
+		head = head->next;
+		temp->next = nullptr;
+		delete temp;
+	}
+	
+	void pop_back(){
+		if(head == nullptr){
+			cout << "List is empty ";
+			return;
+		}
+		
+		if(head->next == nullptr){
+			delete head;
+			head = nullptr;
+			return;
+		}
+		
+		Node* temp = head;
+		while(temp->next->next != nullptr){
+			temp = temp->next;
+		}
+		
+		delete temp->next;
+		temp->next = nullptr;
+		
+	}
+	
+	void insert(int pos, int val){
+    	if(pos <= 0){
+        	cout << "Invalid position\n";
+        	return;
+    	}
+
+    	if(pos == 1){
+        	this->push_front(val);
+        	return;
+    	}
+
+    	Node* n = new Node(val);
+    	Node* current = head;
+    	int idx = 1;
+
+    	while(current != nullptr && idx < pos-1){
+        	current = current->next;
+        	idx++;
+    	}
+
+    	if(current == nullptr){
+        	cout << "Position out of range\n";
+        	delete n; 
+        	return;
+    	}
+
+    	n->next = current->next;
+    	current->next = n;
+	}
+
+	
+	
+	void print(){
+		Node* temp = head;
+		while(temp != nullptr){
+			cout << temp->data << " -> " ;
+			temp = temp->next;
+		}
+		cout << "nullptr\n\n";
+	}
+};
+
+int main(){
+	
+	List ll;
+	ll.push_front(3);
+	ll.push_front(2);
+	ll.push_front(1);
+	ll.push_back(4);
+	ll.push_back(5);
+	ll.print();
+	
+	ll.push_front(100);
+	ll.push_front(101);
+	ll.print();
+	
+	ll.pop_front();
+	ll.print();
+	
+	ll.pop_front();
+	ll.print();
+	
+	ll.pop_back();
+	ll.pop_back();
+	ll.print();
+	
+	
+	ll.insert(4,100);
+	ll.print();
+	 	
+	return 0;
+}
