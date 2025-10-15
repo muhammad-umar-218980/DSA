afd1a6fd63f7b6ec35f65aa311f60f638c965b81
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-10-16 00:49:03 +0500

    Added tree data structure file
    Created Tree.cpp with detailed comments explaining tree concepts, including generic and binary trees.

---
+++ Trees/Tree.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+/*
+    -----------------------------------------------
+                     TREE (Data Structure)
+    -----------------------------------------------
+    A Tree is a non-linear hierarchical data structure 
+    consisting of nodes connected by edges.
+
+    - The topmost node is called the Root.
+    - Each node can have child nodes.
+    - Nodes with no children are called Leaf Nodes.
+    - The connection between nodes is called an Edge.
+    - Trees do not contain any cycles.
+
+    Trees are widely used to represent hierarchical relationships,
+    such as:
+        - File systems (folders and files)
+        - HTML document structure (DOM)
+        - Organization charts
+        - Binary Search Trees (for searching and sorting)
+*/
+
+/*
+    -----------------------------------------------
+                  GENERIC TREE
+    -----------------------------------------------
+    A Generic Tree (also called a General Tree) 
+    is a tree where each node can have any number of children.
+
+    Example:
+             A
+           / | \
+          B  C  D
+             / \
+            E   F
+
+    - Here, node A has 3 children (B, C, D).
+    - Node C has 2 children (E, F).
+    - There is no restriction on how many children a node can have.
+*/
+
+/*
+    -----------------------------------------------
+                 BINARY TREE
+    -----------------------------------------------
+    A Binary Tree is a special type of tree where 
+    each node can have at most two children.
+
+    Example:
+             A
+            / \
+           B   C
+          / \
+         D   E
+
+    - Here, every node has 0, 1, or 2 children.
+    - These children are referred to as the Left Child 
+      and the Right Child.
+
+    ------------------------------------------------
+        The Main focus will be on binary trees
+    ------------------------------------------------
+*/
+
+int main(){
+	
+    return 0;
+}
